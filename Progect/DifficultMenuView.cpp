4e67e79fdf52cc62920cab125e64d30e63518307
Artem <silenko365@gmail.com>
2016-02-19 18:48:37 +0200

    no message

--- Progect/DifficultMenuView.cpp
+++ Progect/DifficultMenuView.cpp
@@ -7,7 +7,8 @@ DifficultMenuView::DifficultMenuView(int width, int height, ALLEGRO_BITMAP *back
 {
 
 }
-void DifficultMenuView::Update() {
+void DifficultMenuView::Update()
+{
 	/*al_draw_bitmap(backgroundImage, 0, 0, 0);
 	al_draw_text(mainFont, al_map_rgb(0, 0, 255), width / 2, 10, ALLEGRO_ALIGN_CENTER, "-=  D R E A M   C A R S =-");
 	al_draw_text(setingsFont, al_map_rgb(255, 0, 0), width / 2, 10, ALLEGRO_ALIGN_CENTER, "DifficultMenu");*/
